35db1d7cff55ae18cc721dd386e6b6e46ec8eef9
michael <mchege78@gmail.com>
2023-03-13 12:31:24 +0300

    Added analytics

--- src/frontend/src/pages/analytics/Analytics.jsx
+++ src/frontend/src/pages/analytics/Analytics.jsx
@@ -1,6 +1,8 @@
 import * as React from "react";
-import { Container, Grid, Paper } from "@mui/material";
+import { Box, Container, Grid, Paper } from "@mui/material";
 import Title from "../../components/title/Title";
+import LineChart from "../../components/lineChart/LineChart";
+import PieChart from "../../components/pieChart/PieChart";
 
 const Analytics = () => {
   return (
@@ -9,6 +11,21 @@ const Analytics = () => {
         title="Analytics"
         subTitle="Manage  your personal finance & budget to meet your personal goal"
       />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          gap: "2rem",
+        }}
+      >
+        <Paper sx={{ height: "30rem" }}>
+          <LineChart />
+        </Paper>
+        <Paper sx={{ height: "20rem" }}>
+          <PieChart />
+        </Paper>
+      </Box>
     </>
   );
 };
